edc307666aa1c392ca504e04c4e5ad31be335586
MahmudulHasanSheshir123 <130019673+MahmudulHasanSheshir123@users.noreply.github.com>
2023-05-10 11:57:17 +0600

    Update sheshir.sh

--- sheshir.sh
+++ sheshir.sh
@@ -1,9 +1,4 @@
 #! /bin/bash
-git clone https://github.com/iatiqul/codefellas.git
-git switch sheshir
-touch node.txt
-git add .
-git commit -m "Add"
-git push -u origin sheshir
+
 echo "If everything is okay then print hello world"
 echo "Hello world"
